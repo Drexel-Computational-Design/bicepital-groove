485449618191de42dd9b0f03944438b97d68352e
Kabilan108 <tko35@drexel.edu>
2021-10-01 01:48:06 -0400

    Debugged code for loading images

--- tests/LoadDir.m
+++ tests/LoadDir.m
@@ -1,3 +1,18 @@
 function LoadDir()
   % Load user-selected image directory and filter out images
+	path = uigetdir();  % Select image directory
+	files = dir(path);  % List all files in directory
+	files = files(~[files.isdir]); % Remove subdirectories from list
+	% Keep only .jpg files
+	I = ~cellfun('isempty', regexp({files.name}, '.jpg$'));
+	files = files(I);
+	% Load all images
+	imgs = cell(numel(files), 1);  % preallocate image storage
+	for i = 1:numel(files)
+		fpath = fullfile(files(i).folder, files(i).name);
+		imgs{i} = imread(fpath);
+	end
+	% Display first image
+	ax = uiaxes;
+	imagesc(ax, imgs{1})
 end
